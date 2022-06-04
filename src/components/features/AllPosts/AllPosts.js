39dedcb8782004b9c1678f935a26e204b1e61e68
Krzysztof Potocki <kpotocki89@gmail.com>
2022-06-04 22:29:43 +0200

    Add Post view

--- src/components/features/AllPosts/AllPosts.js
+++ src/components/features/AllPosts/AllPosts.js
@@ -2,22 +2,23 @@ import { useSelector } from "react-redux";
 import Post from '../../pages/Post/Post';
 import { getAllPosts } from "../../../redux/postsRedux";
 import { NavLink } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 const AllPosts = () => {
 
   const posts = useSelector(state => getAllPosts(state));
 
   return (
-    <article >
-      <div className="d-flex justify-content-between">
+    <Container>
+      <div className="d-flex justify-content-between mb-4">
         <h2>All posts</h2>
-        <button variant="outline-primary" as={NavLink} to="/post/add">Add post</button>
+        <button type="button" className='btn btn-outline-info'>Add post</button>
       </div>
-      <ul>
+      <ul className="d-flex flex-wrap row">
         {posts.map(post => 
           <Post key={post.id} title={post.title} id={post.id} shortDescription={post.shortDescription} author={post.author} publishedDate={post.publishedDate}/>)}
       </ul>
-    </article>
+      </Container>
   );
 };
 
--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -1,9 +1,23 @@
-import styles from './Post.module.scss';
+import { Card } from 'react-bootstrap';
 
-const Post = () => {
+const Post = props => {
   return (
-    <div className={styles.post}>
-      <h1>Post</h1>
+    <div className='col-sm-6 col-lg-4 my-2'>
+      <Card >
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text className='my-0'>
+            <strong>Author:</strong> {props.author}
+          </Card.Text>
+          <Card.Text className='my-0'>
+            <strong>Publised:</strong> {props.publishedDate}
+          </Card.Text>
+          <Card.Text className='my-2'>
+            {props.shortDescription}
+          </Card.Text>
+          <button type="button" className='btn btn-primary'>Read more</button>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
