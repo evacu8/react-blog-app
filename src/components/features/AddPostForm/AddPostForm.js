40d4ba081f71c170e96340f8d1631d7025b02d04
Krzysztof Potocki <kpotocki89@gmail.com>
2022-06-06 20:08:34 +0200

    Add PostForm and use it for AddPostForm

--- src/components/features/AddPostForm/AddPostForm.js
+++ src/components/features/AddPostForm/AddPostForm.js
@@ -1,53 +1,20 @@
-import { useState } from "react";
-import { useDispatch } from 'react-redux';
-import { Container } from "react-bootstrap";
 import { addPost } from '../../../redux/postsRedux';
+import PostForm from "../PostForm/PostForm";
+import { useDispatch } from 'react-redux';
 import { useNavigate } from "react-router-dom";
 
 const AddPostForm = () => {
 
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [publishedDate, setPublishedDate] = useState('');
-  const [shortDescription, setShortDescription] = useState('');
-  const [content, setContent] = useState('');
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    dispatch(addPost({ title, author, publishedDate, shortDescription, content}));
+  const handleSubmit = post => {
+    dispatch(addPost(post));
     navigate("/");
   };
 
   return (
-    <Container className='col-lg-8'>
-      <h1>Add post</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="title" className="form-label">Title</label>
-          <input type="text" className="form-control" id="title" placeholder="Enter Title" value={title} onChange={e => setTitle(e.target.value)} />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="author" className="form-label">Author</label>
-          <input type="text" className="form-control" id="author" placeholder="Enter Author" value={author} onChange={e => setAuthor(e.target.value)} />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="published" className="form-label">Published</label>
-          <input type="date" className="form-control" id="published" placeholder="Published" value={publishedDate} onChange={e => setPublishedDate(e.target.value)} />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="shortDescription" className="form-label">Short description</label>
-          <textarea className="form-control" id="shortDescription" rows="3" placeholder="Enter short description here" value={shortDescription} onChange={e => setShortDescription(e.target.value)} ></textarea>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="MainContent" className="form-label">Main content</label>
-          <textarea className="form-control" id="MainContent" rows="10" placeholder="Enter main content here" value={content} onChange={e => setContent(e.target.value)}></textarea>
-        </div>
-      </form>
-      <button type="button" className='btn btn-primary' onClick={handleSubmit}>Add post</button>
-    </Container>
+    <PostForm action={handleSubmit} actionText='Add post'/>
   );
 
 };
---
+++ src/components/features/PostForm/PostForm.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+
+const PostForm = ({ action, actionText, ...props}) => {
+
+  const [title, setTitle] = useState(props.title || '');
+  const [author, setAuthor] = useState(props.author || '');
+  const [publishedDate, setPublishedDate] = useState(props.publishedDate || '');
+  const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
+  const [content, setContent] = useState(props.content || '');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    action({ title, author, publishedDate, shortDescription, content });
+  };
+
+  return (
+    <Container className='col-lg-8'>
+      <h1>{actionText}</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">Title</label>
+          <input type="text" className="form-control" id="title" placeholder="Enter Title" value={title} onChange={e => setTitle(e.target.value)} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="author" className="form-label">Author</label>
+          <input type="text" className="form-control" id="author" placeholder="Enter Author" value={author} onChange={e => setAuthor(e.target.value)} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="published" className="form-label">Published</label>
+          <input type="date" className="form-control" id="published" placeholder="Published" value={publishedDate} onChange={e => setPublishedDate(e.target.value)} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="shortDescription" className="form-label">Short description</label>
+          <textarea className="form-control" id="shortDescription" rows="3" placeholder="Enter short description here" value={shortDescription} onChange={e => setShortDescription(e.target.value)} ></textarea>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="MainContent" className="form-label">Main content</label>
+          <textarea className="form-control" id="MainContent" rows="10" placeholder="Enter main content here" value={content} onChange={e => setContent(e.target.value)}></textarea>
+        </div>
+      </form>
+      <button type="button" className='btn btn-primary' onClick={handleSubmit}>{actionText}</button>
+    </Container>
+  );
+
+};
+
+export default PostForm;
